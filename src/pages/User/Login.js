b02b6e089256abc93db6c0137ffbb5f862eb99ef
unknown <wardahnabilah33@gmail.com>
2023-06-02 05:34:36 +0700

    Fix login and register styles

--- src/pages/User/Login.js
+++ src/pages/User/Login.js
@@ -7,9 +7,9 @@ export function Login() {
 
   return (
     <section className="w-11/12 py-12 max-w-screen-xl mx-auto">
-        <h1 className="mb-12 text-3xl text-center font-semibold underline underline-offset-8">Login</h1>
+        <h1 className="mb-12 text-3xl text-center font-semibold dark:font-normal underline underline-offset-8">Login</h1>
         <form className="w-11/12 mx-auto text-lg">
-            <div className="my-2 flex flex-col">
+            <div className="my-3 flex flex-col">
                 <label className="mb-1" htmlFor="#loginEmail">Your email</label>
                 <input className="pl-3 py-1 text-lg border-2 bg-slate-50 border-neutral-300 focus:outline-blue-700 rounded-lg dark:bg-gray-700" type="text" id="loginEmail" placeholder="shubham@example.com" />
             </div>
