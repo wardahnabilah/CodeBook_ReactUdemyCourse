2af53d09d8ec4884d2de33a7f9bb75f43ee8cd2e
unknown <wardahnabilah33@gmail.com>
2023-06-01 09:05:43 +0700

    Add filter reducer

---
+++ src/reducers/filterReducer.js
@@ -0,0 +1,23 @@
+export default function filterReducer(state, action) {
+    const {type, payload} = action
+
+    switch(type) {
+        case "PRODUCT_LIST":
+            return
+            
+        case "SORT_BY":
+            return
+
+        case "RATING":
+            return
+
+        case "BEST_SELLER":
+            return
+
+        case "INSTOCK":
+            return
+
+        default:
+            throw new Error("Error")
+    }
+}
